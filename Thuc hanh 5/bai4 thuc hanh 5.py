33726283b0d54bb2fddda1d6618daccc86272429
morganquan <phamquan2410005@gmail.com>
2024-11-29 14:59:38 +0700

    Delete Thuc hanh 5 directory

--- Thuc hanh 5/bai4 thuc hanh 5.py
+++
@@ -1,8 +0,0 @@
-print("Pham Viet Quan Mssv 235752021610022")
-import numpy as np
-x = np.arange(12, 38)
-print("Mảng được tạo:")
-print(x)
-x_reverse = x[::-1]
-print("Mảng đảo ngược:")
-print(x_reverse)
